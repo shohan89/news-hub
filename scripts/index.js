bba1c8aeb18cce4a2997896b7afdfe22bec9ed0d
shoh4n <maffin.killer007@gmail.com>
2025-06-21 17:25:18 +0600

    All categories loaded from the api.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-console.log('JS Loaded!');
+const loadCategories = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/categories`);
+    const data = await res.json();
+    const allCategories = data.data.news_category;
+    
+    const categoryContainer = document.getElementById('category-container');
+    allCategories.forEach((singleCategory)=>{
+        // console.log("🚀 ~ allCategories.forEach ~ singleCategory:", singleCategory)
+        const div = document.createElement('div');
+        div.innerHTML = `<button class="btn">${singleCategory.category_name}</button>`;
+        categoryContainer.appendChild(div);
+    })
+}
+loadCategories();
